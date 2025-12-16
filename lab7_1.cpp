2465eea5e22cf2cf9d08f8c7fec9ad853ae74555
nicklovecat <skykillergamer@gmail.com>
2025-12-16 16:40:33 +0700

    lab7_1 completed

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
-
-    outpot = a - b/c;
-    count << output: << output << \n;
+    float a = 20, b = 30;
+    int c = 4;
+    float output;
+    output = a - (b / (float)c);
+    cout << "output: " << output << endl;
     return 0;
 }
